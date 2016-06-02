3731a79f63a7661fbd41e238b9abc6a4ae628f7c
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2016-06-02 16:45:47 -0700

    cleanup again

--- main/java/com/eternal/dimension/overworld/util/VillagePieceRegister.java
+++ main/java/com/eternal/dimension/overworld/util/VillagePieceRegister.java
@@ -5,10 +5,13 @@ import net.minecraftforge.fml.common.registry.VillagerRegistry;
 
 public class VillagePieceRegister {
 	
-	public static void addVillagePiece(Class className, String structureName) 
-	{ try { 
-		MapGenStructureIO.registerStructure(className, structureName); 
-	} catch (Exception e) {}
+	public static void addVillagePiece(Class className, String structureName) { 
+		try { 
+			MapGenStructureIO.registerStructure(className, structureName); 
+		} 
+		catch (Exception e) {
+
+		}
 	}
 
 	public static void addVillageCreationHandler(VillagerRegistry.IVillageCreationHandler v) { 
